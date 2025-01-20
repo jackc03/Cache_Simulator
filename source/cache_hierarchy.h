d907648c015d46f2046563c8c068997560840179
jcochran66 <jcochran66@gatech.edu>
2025-01-20 17:46:48 -0500

    Pushing changes made last time

--- source/cache_hierarchy.h
+++ source/cache_hierarchy.h
@@ -6,12 +6,16 @@
 
 class Three_Level_Cache_Hierarchy {
     public:
+        // Access times used to calculate AMAT in ns
+        // Default values are 2, 10, 20, 100 respectively
+        const double L1_access_time, L2_access_time, L3_access_time, DRAM_access_time;
         std::shared_ptr<Set_Associative_Cache> L1;
         std::shared_ptr<Set_Associative_Cache> L2;
         std::shared_ptr<Set_Associative_Cache> L3;
 
-        Three_Level_Cache_Hierarchy(uint64 l1_cache_size, uint64 l2_cache_size, uint64 l3_cache_size, uint32 block_size, REPLACEMENT replacement_policy, uint8 associativity) {
-            std::shared_ptr<Statistics> stats(new Statistics());
+        Three_Level_Cache_Hierarchy(uint64 l1_cache_size, uint64 l2_cache_size, uint64 l3_cache_size, uint32 block_size, REPLACEMENT replacement_policy, uint8 associativity) 
+                                    : L1_access_time(L1_access_time), L2_access_time(L2_access_time), L3_access_time(L3_access_time), DRAM_access_time(DRAM_access_time) {
+            // std::shared_ptr<Statistics> stats(new Statistics(), double L1_access_time = 2.0, double L2_access_time = 10.0, double L3_access_time = 20, double DRAM_access_time = 100.0);
             
             L3 = std::shared_ptr<Set_Associative_Cache>(new Set_Associative_Cache(LEVEL::L3, l3_cache_size, block_size, replacement_policy, associativity, stats, nullptr));
             L2 = std::shared_ptr<Set_Associative_Cache>(new Set_Associative_Cache(LEVEL::L2, l2_cache_size, block_size, replacement_policy, associativity, stats, std::dynamic_pointer_cast<Cache>(L3)));
